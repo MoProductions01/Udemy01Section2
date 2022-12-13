f80d0b58530111ea2fafea5ede449e52bc85d771
MoProductions <moproductions01@gmail.com>
2022-12-13 14:11:24 -0700

    End of section 7

---
+++ Assets/Scripts/LaunchManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class LaunchManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ConnectToPhotonServer()
+    {
+        if(PhotonNetwork.IsConnected == false)
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster() // master server is photon server
+    {
+        Debug.Log("NickName: " + PhotonNetwork.NickName + " is connected to Photon Servers (Master)");
+    }
+
+    public override void OnConnected() // will be called before OnConnectedToMaster because this is the internet connect check
+    {
+        Debug.Log("Connected to internet");
+    }
+}
