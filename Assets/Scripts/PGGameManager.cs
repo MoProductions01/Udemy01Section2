5caf2687646e30903e0badac35c043253e2ae6b4
MoProductions <moproductions01@gmail.com>
2022-12-15 14:43:41 -0700

    End of section

--- Assets/Scripts/PGGameManager.cs
+++ Assets/Scripts/PGGameManager.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.SceneManagement;
 
 public class PGGameManager : MonoBehaviourPunCallbacks
 {
     [SerializeField]
     GameObject playerPrefab;
+
+    public static PGGameManager instance; // make this a singleton so there's only one instance in the scene at a time // monote
+
+    public void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -37,5 +52,16 @@ public class PGGameManager : MonoBehaviourPunCallbacks
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         Debug.Log("OnPlayerEnteredRoom() newPlayer NickName: " + newPlayer.NickName + " joined room: " + PhotonNetwork.CurrentRoom.Name + " room count now: " + PhotonNetwork.CurrentRoom.PlayerCount);
+    }
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene("GameLauncher");
+    }
+
+    public void LeaveRoom()
+    {
+        Debug.Log("LeaveRoom()");
+        PhotonNetwork.LeaveRoom();
     }
 }
--- Assets/Scripts/TakingDamage.cs
+++ Assets/Scripts/TakingDamage.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
 
-public class TakingDamage : MonoBehaviour
+public class TakingDamage : MonoBehaviourPunCallbacks
 {
-    public float health;
+    [SerializeField]
+    Image healthBar;
+
+    private float health;
+    public float startHealth = 100f;
 
     // Start is called before the first frame update
     void Start()
     {
-        health = 100f;
+        health = startHealth;
+
+        healthBar.fillAmount = health/startHealth;
     }
 
     // Update is called once per frame
@@ -24,5 +31,28 @@ public class TakingDamage : MonoBehaviour
     {
         health -= _damage;
         Debug.Log("TakeDamage() health: " + health);
+        healthBar.fillAmount = health / startHealth;
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void OnGUI()
+    {
+        if (photonView.IsMine == false) return;
+
+        if(GUI.Button(new Rect(0,0,300,100), PhotonNetwork.NickName + ": take 1000 dmg"))
+        {
+            TakeDamage(1000);
+        }
+    }
+
+    void Die()
+    {
+        if(photonView.IsMine)
+        {
+            PGGameManager.instance.LeaveRoom();
+        }
     }
 }
