2e894fd9a5923dbe4720282387b176ede6e0670b
MoProductions <moproductions01@gmail.com>
2022-12-15 12:41:15 -0700

    End of section 20

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-
+using TMPro;
 
 public class PlayerSetup : MonoBehaviourPunCallbacks
 {
     [SerializeField]
-    GameObject FPSCamera;        
+    GameObject FPSCamera;
+
+    [SerializeField]
+    TextMeshProUGUI playerNameText;
 
     // Start is called before the first frame update
     void Start()
@@ -22,11 +25,15 @@ public class PlayerSetup : MonoBehaviourPunCallbacks
             transform.GetComponent<MovementController>().enabled = false;
             FPSCamera.GetComponent<Camera>().enabled = false;                 
         }
+
+        SetPlayerUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    void SetPlayerUI()
     {
-        
+        if(playerNameText != null)
+        {
+            playerNameText.text = photonView.Owner.NickName;
+        }        
     }
 }
